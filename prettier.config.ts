4b96769160be83ffc6e9bbb1d0e76e33f54c5e4b
spanwite <spanwite@ya.ru>
2026-03-19 13:23:58 +0300

    create empty vite/preact project

---
+++ prettier.config.ts
@@ -0,0 +1,13 @@
+import { type Config } from 'prettier';
+
+const config: Config = {
+  arrowParens: 'always',
+  singleQuote: true,
+  jsxSingleQuote: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  semi: true,
+};
+
+export default config;
